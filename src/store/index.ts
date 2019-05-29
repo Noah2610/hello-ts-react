352dbc527d0b348fa350bf5fc9a7f24ef95a250b
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-05-29 21:02:21 +0200

    Fix spelling mistake

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,7 +14,7 @@ export const configureStore = (): Store => {
     );
 };
 
-// Middleware for chrom devtools redux extension
+// Middleware for chrome devtools redux extension
 const devtoolsExtensionMiddleware = (): any =>
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__();
